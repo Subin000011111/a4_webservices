13600a0d77996e6dc2c36dc789280ddffa25f9b4
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 15:46:37 -0400

    Add schema and seed data for Book table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO BOOK(TITLE, AUTHOR_NAME, PRICE, QUANTITY)
+VALUES
+    ('The Alchemist', 'Paulo Coelho', 15.99, 10),
+    ('1984', 'George Orwell', 12.49, 5),
+    ('The Hobbit', 'J.R.R. Tolkien', 20.00, 7);
