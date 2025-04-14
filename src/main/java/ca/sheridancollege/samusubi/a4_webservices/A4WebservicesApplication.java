6444a8a899ad9d3e32c18c8dfad3a83fea74ddd8
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 15:32:39 -0400

    project setup

---
+++ src/main/java/ca/sheridancollege/samusubi/a4_webservices/A4WebservicesApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.samusubi.a4_webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4WebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A4WebservicesApplication.class, args);
+    }
+
+}
